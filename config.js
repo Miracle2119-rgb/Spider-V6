96c25a2181da8d0559f191bc8df32f0a499b183a
Miracle2119-rgb <sadipsbot@gmail.com>
2024-08-30 01:50:45 +0100

    Update config.js

--- config.js
+++ config.js
@@ -2,21 +2,21 @@
 
 
 require("./all/module")
-global.owner = "254742491666" 
-global.namaCreator = "𝕶𝖎𝖓𝖌 𝕾𝖆𝖒" 
+global.owner = "2347049602299" 
+global.namaCreator = "𝐃𝐄𝐌𝐎𝐍 𝐌-𝐂𝐑𝐎𝐒𝐒" 
 global.autoJoin = false 
 global.antilink = false 
 global.versisc = '6.0' 
-global.codeInvite = "https://whatsapp.com/channel/0029VaaqaSp0LKZDuwe5SI3e"
-global.isLink = 'https://whatsapp.com/channel/0029VaaqaSp0LKZDuwe5SI3e'
+global.codeInvite = "https://Wa.me/2347049602299?text=Boss%20ＫＩＮＧ🍀Ｍ-ＣＲＯＳＳ%20my%20name%20is___%20and%20I've%20saved%20your%20contact%20you%20should%20please%20save%20mine%20too%20%20✅"
+global.isLink = 'https://Wa.me/2347049602299?text=Boss%20ＫＩＮＧ🍀Ｍ-ＣＲＯＳＳ%20my%20name%20is___%20and%20I,ve%20saved%20your%20contact%20you%20should%20please%20save%20mi'
 global.sessionName = 'session'
 global.thumb = fs.readFileSync("./thumb.png") 
 global.audionya = fs.readFileSync("./all/sound.mp3") 
-global.packname = "𝕶𝖎𝖓𝖌 𝕾𝖆𝖒" 
-global.author = "𝐒𝚸𝚰𝐃𝚵𝚪-𝛁6" 
+global.packname = "𝐃𝐄𝐌𝐎𝐍 𝐌-𝐂𝐑𝐎𝐒𝐒" 
+global.author = "𝐃𝐄𝐌𝐎𝐍-𝛁6" 
 global.jumlah = "5" 
-global.namabot = "𝐒𝚸𝚰𝐃𝚵𝚪-𝛁6" 
-global.domain = 'https://kingsam.sellerpanelerol.me' // your domian
+global.namabot = "𝐃𝐄𝐌𝐎𝐍-𝛁6" 
+global.domain = 'https://mcrossbot.me.id' // your domian
 global.apikey = 'ptla_VRxkhvalZQ3EhHyAax9QYxgT5cbsx2MrO69gBpVZmmd' // Isi Apikey Plta Lu
 global.capikey = 'ptlc_sQ3d4GxCiztyV5DbhUJSWtvnSCyKzeMXoWIr2lVyqal' // Isi Apikey Pltc Lu
 global.eggsnya = '15' // id eggs yang dipakai
